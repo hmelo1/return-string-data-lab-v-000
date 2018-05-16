e3e1c22e6b6e63554e0fe0a2b9d430d75f41414d
Eddie Melo <heriberto@melo.nyc>
2018-05-16 00:07:21 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,32 +1,37 @@
 class ProductsController < ApplicationController
-
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
-
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
-
-  def index
-    @products = Product.all
-  end
+  before_action :set_product, only: [:show, :description, :inventory]
 
   def new
     @product = Product.new
   end
 
   def create
-    @product = Product.create(product_params)
-    @product.save
+    product = Product.create(product_params)
     redirect_to products_path
+  end
+
+  def show
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def description
+    render plain: @product.description
+  end
+
+  def inventory
+    render plain: @product.inventory > 0
   end
 
   private
 
+  def set_product
+    @product = Product.find_by(id: params[:id])
+  end
+
   def product_params
-    params.require(:product).permit(:name, :description)
+    params.require(:product).permit(:name, :description, :price, :inventory)
   end
 end
